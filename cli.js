75574dde4af7126f7520c082e488e982ae322d33
myf <myf@worldtalks.org>
2022-06-29 18:12:43 +0000

    refactor cli, lights, and changed init-lights to simple

--- cli.js
+++ cli.js
@@ -8,27 +8,30 @@ const cli = async () => {
             await run();
             break;
         case "lights":
-            switch(argv.status) {
-                case "init":
-                    await lights.init_seq();
-                    break;
-                case "on":
-                    await lights.connected();
-                    break;
-                case "off":
-                    await lights.disconnected();
-                    break;
-                default:
-                    console.log(`you sure ${argv.status}`);
-                    console.log(argv)
-                    break;
-
-            }
+            await lights_switch(argv.status);
             break;
         default:
             console.log(`unrecognized action ${argv._[0]}`)
             console.log(argv);
             break;
+    }
+}
+
+const lights_switch = async (sta) => {
+    switch(sta) {
+	case "init":
+	    await lights.init_seq();
+	    break;
+	case "on":
+	    await lights.connected();
+	    break;
+	case "off":
+	    await lights.disconnected();
+	    break;
+	default:
+	    console.log(`you sure ${sta}`);
+	    console.log(argv)
+	    break;
     }
 }
 
