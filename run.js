809c2601f3d441719dfe89c62192c880207a0b09
myf <myf@worldtalks.org>
2022-07-13 03:27:35 +0000

    refactor run routine in order to send status to the frontend

--- run.js
+++ run.js
@@ -6,28 +6,45 @@ const fs = require('fs/promises');
 const run = async () => {
     const content = await speedtest();
     const result = await publish(content);
+    const quality = get_quality(content);
     await fs.writeFile('/home/giga/GIGANode/Frontend/data/wat.json', 
                        JSON.stringify([result]));
-    await indicate(content);
+    await fs.writeFile('/home/giga/GIGANode/Frontend/data/status.json',
+                       JSON.stringify({status: "Connected",
+                                       quality: quality})); 
+    await indicate(quality);
     return;
 }
 
-const indicate = async (content) => {
+const get_quality = (content) => {
     const speed_mb = content.download / 10**6;
+    let status;
     switch (true) {
         case speed_mb < 0.1:
-            await lights.low();
+            status = 'low';
             break;
         case (speed_mb > 0.1 && speed_mb < 20):
-            await lights.med();
+            status = 'medium';
             break;
         case speed_mb > 20:
-            await lights.high();
+            status = 'good'
             break;
         default:
             console.error(`content is unparsable ${content}`);
+            status = 'error'
             break;
     }
+    return status;
+}
+const quality_map = {
+    good: lights.high,
+    medium: lights.med,
+    low: lights.low,
+}
+
+const indicate = async (quality) => {
+    const func = quality_map[quality];
+    await func();
     return;
 }
 
